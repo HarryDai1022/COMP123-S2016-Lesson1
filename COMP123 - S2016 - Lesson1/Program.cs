fd71f14c831610c67aab591341daaa8e2d0a097e
Harry Dai <dz655618@live.com>
2016-05-11 17:36:03 -0400

    COMP123-S2016-Lesson1

---
+++ COMP123 - S2016 - Lesson1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123___S2016___Lesson1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldToConsole();
+        }
+
+        //This is my HElloWorld method
+        //method HelloWorldToConsole
+        //return (string) HelloWorldString
+        public static string HelloWorldToConsole()
+        {
+            string HelloWorldString = "Hello, World!";
+            Console.WriteLine(HelloWorldString);
+            return HelloWorldString;
+        }
+    }
+}
---
+++ HelloWorldTestProject/HelloWorldUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTestProject
+{
+    [TestClass]
+    public class HelloWorldUnitTest
+    {
+        [TestMethod]
+        public void HelloWorldToConsole()
+        {
+            //Arrange
+            string expectedResult = "Hello, World!";
+
+            //Act
+            string result = COMP123___S2016___Lesson1.Program.HelloWorldToConsole();
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+
+        }
+    }
+}
